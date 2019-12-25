202b3c4b5aa124b9ef46b9fdeca80e9b2efa94f0
JayJay-H <0911aza@naver.com>
2019-12-25 11:27:09 +0900

    Token이 보여지지 않도록 수정

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
 import discord
 import asyncio
+import info
 from project_scrapper import recent_info
 
 from discord import channel
 
-TOKEN = 'NjU4OTczOTA1NDM1NzU0NTI2.XgHl9A.VevXgn0wOHpP48J_NecER1eluwA'
-HELP = "[$showproject] --> 사업단 소식"
+TOKEN = info.get_token()
+HELP = "[명령어 모음]" \
+       "\n[$updateall] --> 모든 정보 받아오기" \
+       "\n[$showproject] --> 사업단소식" \
+       "\n[$showbachelor] --> 학사공지" \
+       "\n[$shownotice] --> 일반소식" \
+       "\n[$showjob] --> 취업정보" \
+       "\n[$showcse] --> 우리학부 News"
 client = discord.Client()
 
 
@@ -20,19 +27,17 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('$help'):
-        channel = message.channel
-        await channel.send(HELP)
+        h_channel = message.channel
+        await h_channel.send(HELP)
 
     if message.content.startswith('$showproject'):
-        channel = message.channel
-        Info = recent_info()
-        index = Info['index']
-        date = Info['date']
-        title = Info['title']
-        content = Info['content']
-        show = f"[사업단소식]\n[번호]: {index}          [작성일]: {date}\n[제목]: {title}\n-------------\n{content}"
-        await channel.send(show)
-
-
+        p_channel = message.channel
+        project_info = recent_info()
+        index = project_info['index']
+        date = project_info['date']
+        title = project_info['title']
+        content = project_info['content']
+        show = f"[사업단소식]\n[번호]: {index} [작성일]: {date}\n[제목]: {title}\n-------------\n{content}"
+        await p_channel.send(show)
 
 client.run(TOKEN)
